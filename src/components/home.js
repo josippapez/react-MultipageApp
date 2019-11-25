de8d5d7005c4d8935fe044c7909455bb0eb0cb6a
josippapez <papezjosip@gmail.com>
2019-11-25 15:51:13 +0100

    added axios to home

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React,{Component} from 'react';
+import Axios from 'axios';
 
-function Home (){
+class Home extends Component{
+    state={
+        posts:[]
+    }
+    componentDidMount(){
+        Axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(res=>{
+                console.log(res);
+                this.setState({
+                    posts:res.data.slice(0,20)
+                })
+            });
+    }
+    render(){
+        const {posts}=this.state;
+        const postList=posts.length ? (posts.map(post=>{
+            return(
+                <div className="post card" key={post.id}>
+                    <div className="content">
+                        <span className="card-title">{post.title}</span>
+                        <p>{post.body}</p>
+                    </div>
+                </div>
+            )
+        })): (
+            <div className="center">No posts yet</div>)  
     return (
         <div className="container">
             <h4 className="center">Home</h4>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quo cupiditate omnis numquam repellat ratione dolor fugit id unde voluptatibus, dignissimos hic autem sequi cumque iure voluptate corporis atque itaque distinctio.</p>
+            {postList}
         </div>
     )
+    }
 }
 
 export default Home;
